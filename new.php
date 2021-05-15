eb833909e336acecb7e5a6818535ad17301707eb
ubanitc <57474291+ubanitc@users.noreply.github.com>
2021-05-15 22:49:48 +0100

    Update new.php

--- new.php
+++ new.php
@@ -22,22 +22,22 @@ header("Access-Control-Allow-Headers: *");
         
    
     //Instantiation and passing `true` enables exceptions
-    $mail = new PHPMailer(true);
+   $mail = new PHPMailer(true);
     
     try {
         //Server settings
         $mail->SMTPDebug = 3;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = '9b74bee33566d6';                     //SMTP username
-        $mail->Password   = '65cd092ce93ebf';                               //SMTP password
+        $mail->Username   = 'billharris7431@gmail.com';                     //SMTP username
+        $mail->Password   = 'kfdtqayhuvyzbujk';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
         //Recipients
-        $mail->setFrom('trencytech@mailgun.org');
-        $mail->addAddress('tochukwu.ubani@gmail.com');     //Add a recipient
+        $mail->setFrom('billharris7431@gmail.com',"ubani");
+        $mail->addAddress('tochukwu.ubani@yahoo.com');     //Add a recipient
      
         //Content
         $mail->isHTML(true);                                  //Set email format to HTML
@@ -51,6 +51,8 @@ header("Access-Control-Allow-Headers: *");
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
  
+
+
 
 
 ?>

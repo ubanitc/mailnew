c1d0e815340841f90d89210dcf3e8027616bc321
ubanitc <57474291+ubanitc@users.noreply.github.com>
2021-05-15 18:15:24 +0100

    Update new.php

--- new.php
+++ new.php
@@ -6,6 +6,8 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
   
     
+ 
+    
     //Load Composer's autoloader
     require 'vendor/autoload.php';
     
@@ -29,15 +31,15 @@ header("Access-Control-Allow-Headers: *");
         //Server settings
         $mail->SMTPDebug = 3;                      //Enable verbose debug output
         $mail->isSMTP();                                            //Send using SMTP
-        $mail->Host       = 'smtp.mailgun.org';                     //Set the SMTP server to send through
+        $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-        $mail->Username   = 'postmaster@sandbox8d8ad03e47b846e7904b83c6151e5b6a.mailgun.org';                     //SMTP username
-        $mail->Password   = '9b8640c6cb643bdd8d1b078b8e417efc-602cc1bf-8c6264a2';                               //SMTP password
+        $mail->Username   = '9b74bee33566d6';                     //SMTP username
+        $mail->Password   = '65cd092ce93ebf';                               //SMTP password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
         $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
     
         //Recipients
-        $mail->setFrom('postmaster@sandbox8d8ad03e47b846e7904b83c6151e5b6a.mailgun.org');
+        $mail->setFrom('trencytech.mailgun.org');
         $mail->addAddress('tochukwu.ubani@gmail.com');     //Add a recipient
      
         //Content
